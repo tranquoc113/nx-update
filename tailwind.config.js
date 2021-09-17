1ca1b0c090629eaa53b4dd15c5bf549280c7213c
quoc <tranquoc631@gmail.com>
2021-09-18 00:44:54 +0700

    add project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./apps/myapp/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
